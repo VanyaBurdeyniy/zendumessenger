3a1e0de74cf402521dc688a37aaafc437f1f2475
yurii.shevtsov <gtament@gmail.com>
2016-05-05 18:30:22 +0300

    replace function

--- app/components/userBox.js
+++ app/components/userBox.js
@@ -1,22 +1,18 @@
-'use strict';
-
 export default
-
 class UserBox extends React.Component {
-  constructor( props ) {
-    super( props );
+  constructor(props) {
+    super(props)
   }
   selectSelf() {
-    this.props.selectUser( this.props.id )
+    this.props.selectUser(this.props.id)
   }
   render() {
     return (
-      <li onClick={this.selectSelf.bind( this )}
-          className={"clearfix userBox" + (this.props.active ? ' active' : '')} >
-        <div className="about">
-          <div className="name">{this.props.name}</div>
-        </div>
-      </li>
-    );
+        <li onClick={this.selectSelf.bind(this)} className={"clearfix userBox" + (this.props.active ? ' active' : '')} >
+          <div className="about">
+            <div className="name">{this.props.name}</div>
+          </div>
+        </li>
+    )
   }
 }
